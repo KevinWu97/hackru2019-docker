2f89c0455682dba93ce894401c52b04ce8a8b9db
KevinWu97 <kevwu1997@gmail.com>
2019-10-19 21:31:36 +0000

    updating listener

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/KevinWu", (req, res) => {
+app.get("/", (req, res) => {
   return res.json({ name: "Hello World!" });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log('Example app listening at http:/localhost:%s', port);
+});
